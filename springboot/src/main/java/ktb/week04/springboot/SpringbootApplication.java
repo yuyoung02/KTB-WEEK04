d2f0bf8399160bb80fca275a125ca1d616a23883
keryn.hwang <hyyy8810@gmail.com>
2026-08-04 00:52:00 +0900

    bugFix : 시간대 변경

--- springboot/src/main/java/ktb/week04/springboot/SpringbootApplication.java
+++ springboot/src/main/java/ktb/week04/springboot/SpringbootApplication.java
@@ -3,10 +3,13 @@ package ktb.week04.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class SpringbootApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 		SpringApplication.run(SpringbootApplication.class, args);
 	}
 
